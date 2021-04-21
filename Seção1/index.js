bfdd5123469b1414087336bd599f9a6dae1af76c
Vinicius Borges Todeschini <75043020+ViniciusBorgesTodeschini@users.noreply.github.com>
2021-04-21 18:32:22 -0300

    Update index.js

--- Seção1/index.js
+++ Seção1/index.js
@@ -7,11 +7,26 @@ for (let i = 0; i <= 10; i++)
     //console.log( i  + " X " + n + " = " + (i*n));
 }
 
-function somar(x1,x2)
+function calc(x1,x2, operator)
 {
-    return x1 + x2;
+    return eval(`${x1}${operator}${x2}`);
 }
 
-let resultado = somar(1,2);
+let resultado = calc(1,2,'-');
 
 console.log(resultado);
+
+//Função anonima
+/**
+(function (x1,x2, operator){
+    return eval(`${x1}${operator}${x2}`);
+})(1,2,'+');
+*/
+
+//Arrow function
+/**
+let calc2 = (x1,x2, operator) => {
+    return eval(`${x1}${operator}${x2}`);
+}
+ */
+
