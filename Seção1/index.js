b8145cb44dd72100d22318ec7c5f14de3b7af1b0
Vinicius Borges Todeschini <75043020+ViniciusBorgesTodeschini@users.noreply.github.com>
2021-04-21 19:28:04 -0300

    Update index.js

--- Seção1/index.js
+++ Seção1/index.js
@@ -30,6 +30,7 @@ let calc2 = (x1,x2, operator) => {
 }
 */
 
+//Eventos DOM
 window.addEventListener('focus',event => {
     console.log("focus");
 })
@@ -38,3 +39,17 @@ document.addEventListener('click',event => {
     console.log("click");
 })
 
+let agora = new Date(); //variavel recebe um novo objeto data
+console.log(agora.getDate()); //pega o dia da data
+console.log(agora.getFullYear()); //pega o ano da data em 4 digitos
+console.log(agora.getMonth()); //pega o mês da data
+console.log(agora.toLocaleDateString("pt-BR")); //traz a data no formato da região do usuário
+
+let carros = ["palio","uno","toro",98, true, new Date()]; //array com objetos de vários tipos 
+console.log(carros); //retorna o array
+console.log(carros.length); //retorna quantos itens tem o array
+console.log(carros[0]); //retorna o valor a primeira posição do array
+
+carros.forEach(function(value,index){ 
+    console.log(index,value);
+})
