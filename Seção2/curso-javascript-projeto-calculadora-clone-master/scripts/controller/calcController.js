00811d2b9d853b999489f5c802f9ce2067eac370
Vinicius Borges Todeschini <75043020+ViniciusBorgesTodeschini@users.noreply.github.com>
2021-04-24 15:07:27 -0300

    update

--- Seção2/curso-javascript-projeto-calculadora-clone-master/scripts/controller/calcController.js
+++ Seção2/curso-javascript-projeto-calculadora-clone-master/scripts/controller/calcController.js
@@ -3,6 +3,17 @@ class CalcController {
     constructor(){
         this._displayCalc = "0";
         this._currentDate = "";
+        this.initialize();
+    }
+
+    initialize(){
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        displayCalcEl.innerHTML = "3";
+        dateEl.innerHTML = "01/05/2021";
+        timeEl.innerHTML = "15:01";
     }
 
     get displayCalc(){
