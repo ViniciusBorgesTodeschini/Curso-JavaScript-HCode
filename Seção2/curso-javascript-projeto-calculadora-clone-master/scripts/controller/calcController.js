6c1516cb31665eaf0af93bcef787ab2d1b457caf
Vinicius Borges Todeschini <75043020+ViniciusBorgesTodeschini@users.noreply.github.com>
2021-04-24 15:36:41 -0300

    update

--- Seção2/curso-javascript-projeto-calculadora-clone-master/scripts/controller/calcController.js
+++ Seção2/curso-javascript-projeto-calculadora-clone-master/scripts/controller/calcController.js
@@ -1,31 +1,47 @@
 class CalcController {
 
     constructor(){
-        this._displayCalc = "0";
+        this._locale = 'pt-BR';
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
         this._currentDate = "";
         this.initialize();
     }
 
     initialize(){
-        let displayCalcEl = document.querySelector("#display");
-        let dateEl = document.querySelector("#data");
-        let timeEl = document.querySelector("#hora");
-
-        displayCalcEl.innerHTML = "3";
-        dateEl.innerHTML = "01/05/2021";
-        timeEl.innerHTML = "15:01";
+        setInterval(()=>{
+            this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+            this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+        }, 1000);
     }
 
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value){
+        return this._timeEl.innerHTML = value;
+    }    
+
+    get displayDate(){
+        return this._dateEl.innerHTML;    
+    }
+    
+    set displayDate(value){
+        return this._dateEl.innerHTML = value;    
+    }    
+
     get displayCalc(){
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
     }
 
     set displayCalc(value){
-        this._displayCalc = value;
+        this._displayCalcEl.innerHTML = value;
     }
 
     get currentDate(){
-        return this._currentDate;
+        return new Date();
     }
 
     set currentDate(value){
